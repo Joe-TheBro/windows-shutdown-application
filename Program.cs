de7be9b59334d8a50de967d2103316aaeb2b030f
Joe-TheBro <hammj00@gmail.com>
2018-07-18 03:16:33 -0500

    C#
    All the C#

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+
+namespace Google_Update
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Continue?");
+            string op1 = Console.ReadLine();
+            if(op1 == "Yes")
+            {
+                // Get host name
+                String strHostName = Dns.GetHostName();
+
+                // Find host by name
+                IPHostEntry iphostentry = Dns.GetHostEntry(strHostName);
+
+                // Enumerate IP addresses
+                foreach (IPAddress ipaddress in iphostentry.AddressList)
+                {
+                    Process commandProcess = new Process();
+                    try
+                    {
+                        commandProcess.StartInfo.FileName = "cmd.exe";
+                        commandProcess.StartInfo.UseShellExecute = false;
+                        commandProcess.StartInfo.CreateNoWindow = true;
+                        commandProcess.StartInfo.RedirectStandardError = true;
+                        commandProcess.StartInfo.RedirectStandardInput = true;
+                        commandProcess.StartInfo.RedirectStandardOutput = true;
+                        commandProcess.Start();
+                        commandProcess.StandardInput.WriteLine("shutdown /r /m " + ipaddress + " /t 200 /f");
+                        commandProcess.StandardInput.WriteLine("exit");
+                        for (; !commandProcess.HasExited;)//wait executed  
+                        {
+                            System.Threading.Thread.Sleep(1);
+                        }
+                        //error output  
+                        string tmpout = commandProcess.StandardError.ReadToEnd();
+                        string tmpout1 = commandProcess.StandardOutput.ReadToEnd();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    finally
+                    {
+                        if (commandProcess != null)
+                        {
+                            commandProcess.Dispose();
+                            commandProcess = null;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
